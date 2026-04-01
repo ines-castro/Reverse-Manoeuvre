4b564980ca506c86a2ff8d53525eff316d116bef
inescastro <ines.valentedecastro@gmail.com>
2026-04-01 12:31:29 +0100

    Save plots directly to assests folder

--- path_graph.py
+++ path_graph.py
@@ -1,14 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = np.genfromtxt("Reverse-Manoeuvre/path_points.csv", delimiter=",", skip_header=1)
+data = np.genfromtxt("path_points.csv", delimiter=",", skip_header=1)
 x_coords = data[:, 0]
 y_coords = data[:, 1]
 
 # Draw it
-plt.figure(figsize=(10, 6))
-plt.plot(x_coords, y_coords, 'b-')
+plt.style.use('dark_background')
+plt.figure()
+plt.plot(x_coords, y_coords, color="#98FBCB")
 plt.axis('equal')
-plt.grid(True)
-plt.title("Path of the Cart")
+plt.grid(True, which='both', linewidth=0.5, color='gray')
+plt.title("Path")
+plt.xlabel("X [m]")
+plt.ylabel("Y [m]")
+
+# Save it
+plt.savefig("assets/standard_path.png", facecolor=plt.gcf().get_facecolor(), bbox_inches='tight')
+
+# Show it
 plt.show()
+
